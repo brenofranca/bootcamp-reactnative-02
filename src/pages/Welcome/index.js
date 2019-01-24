171714fa78fbf79a3d6842597567c17dc0832a4d
Breno França <franciscobreno.si@gmail.com>
2019-01-24 16:02:19 -0300

    login_usuario: adicionado o recurso de realizar login

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,34 +1,97 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import api from '~/services/api';
 
 import {
-  View, Text, TextInput, TouchableOpacity, StatusBar,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StatusBar,
+  AsyncStorage,
+  ActivityIndicator,
 } from 'react-native';
 
 import styles from './styles';
 
-const Welcome = () => (
-  <View style={styles.container}>
-    <StatusBar barStyle="light-content" />
+class Welcome extends Component {
+  static propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func.isRequired,
+    }).isRequired,
+  };
 
-    <Text style={styles.title}>Bem-vindo</Text>
-    <Text style={styles.text}>
-      Para continuar, precisamos que você informe seu usuário do Github
-    </Text>
+  state = {
+    loading: false,
+    username: '',
+  };
 
-    <View style={styles.form}>
-      <TextInput
-        style={styles.input}
-        autoCapitalize="none"
-        autoCorrect={false}
-        placeholder="Informe seu usuário"
-        underlineColorAndroid="transparent"
-      />
+  componentWillUnmount() {
+    this.setState({ loading: false });
+  }
 
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Prosseguir</Text>
-      </TouchableOpacity>
-    </View>
-  </View>
-);
+  checkUserExists = async (username) => {
+    const user = await api.get(`/users/${username}`);
+
+    return user;
+  };
+
+  saveUser = async (username) => {
+    await AsyncStorage.setItem('@Githuber:username', username);
+  };
+
+  signIn = async () => {
+    const { username } = this.state;
+    const { navigation } = this.props;
+
+    this.setState({ loading: true });
+
+    try {
+      await this.checkUserExists(username);
+
+      await this.saveUser(username);
+
+      navigation.navigate('Repositories');
+    } catch (err) {
+      console.log('User not Found!');
+    }
+  };
+
+  render() {
+    const { username, loading } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+
+        <Text style={styles.title}>Bem-vindo</Text>
+
+        <Text style={styles.text}>
+          Para continuar, precisamos que você informe seu usuário do Github
+        </Text>
+
+        <View style={styles.form}>
+          <TextInput
+            style={styles.input}
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="Informe seu usuário"
+            underlineColorAndroid="transparent"
+            onChangeText={text => this.setState({ username: text })}
+            value={username}
+          />
+
+          <TouchableOpacity style={styles.button} onPress={this.signIn}>
+            {loading ? (
+              <ActivityIndicator size="small" color="#FFF" />
+            ) : (
+              <Text style={styles.buttonText}>Prosseguir</Text>
+            )}
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
 
 export default Welcome;
