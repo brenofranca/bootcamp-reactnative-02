e028c1c90b212e6b26d1dfdef16bca6ad9380ca8
Breno França <franciscobreno.si@gmail.com>
2019-01-24 21:05:17 -0300

    user_session: adicionado o recurso de logout

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
 import { View, Text } from 'react-native';
+
+import Header from '~/components/Header';
 
 // import styles from './styles';
 
 const Repositories = () => (
   <View>
-    <Text>Logged</Text>
+    <Header title="Repositórios" />
   </View>
 );
 
